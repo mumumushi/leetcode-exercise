785d7f9063ff9eb9fef8073c7dae1a325ffcbdbc
mumumushi <yuanchenbiao@outlook.com>
2019-01-15 21:50:06 +0800

    添加longest_palindromic_substring.cpp

---
+++ src/longest_palindromic_substring.cpp
@@ -0,0 +1,25 @@
+/**
+ * Given a string s, find the longest palindromic substring in s. 
+ * You may assume that the maximum length of s is 1000.
+ * Example 1:
+ *  Input: "babad"
+ *  Output: "bab"
+ * Note: "aba" is also a valid answer.
+ * Example 2:
+ *  Input: "cbbd"
+ *  Output: "bb"
+*/
+#include<iostream>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        
+    }
+};
+
+int main() {
+    
+    return 0;
+}
