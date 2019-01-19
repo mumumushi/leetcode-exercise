6ec3d0aa74000570dfe9cfe9c5f01528d8a5b6e2
mumumushi <yuanchenbiao@outlook.com>
2019-01-19 13:35:54 +0800

    Done反转正整数

--- src/reverse_integer.cpp
+++ src/reverse_integer.cpp
@@ -19,13 +19,31 @@ using namespace std;
 class Solution {
 public:
     int reverse(int x) {
-        int ans = x;
+        if (x == INT32_MIN)    return 0;                     //因为要按正数处理，先剔除一个溢出的情况
+        int ans = 0;
+        bool isNegative = false;
+        bool mayBeOverflow = false;
+        if (x < 0)  {                                        //负数按正数处理
+            isNegative = true;
+            x = -1 * x;
+        }
+        if (x/1000000000 != 0)    mayBeOverflow = true;      //假如是十位数，存在overflow的可能
+
+        while (x != 0) {
+            if (mayBeOverflow)                               //检测是否将要overflow
+                if ((ans>INT32_MAX/10) || (ans==INT32_MAX/10 && x%10>7))    return 0;
+
+            ans = 10*ans + x%10;
+            x = x / 10;
+        }
+
+        if (isNegative)    ans = -1 * ans;
         return ans;
     }
 };
 
 int main() {
-    int x = 123;
+    int x = 120;
     cout << x << endl;
     cout << Solution().reverse(x) << endl;
     return 0;
