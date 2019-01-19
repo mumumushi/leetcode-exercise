2fb6c4a4b34d8f529f73f3ba886f802a291652fc
mumumushi <yuanchenbiao@outlook.com>
2019-01-19 22:53:22 +0800

    Done 回文数，给大神下跪

--- src/palindrome_number.cpp
+++ src/palindrome_number.cpp
@@ -25,12 +25,50 @@ using namespace std;
 class Solution {
 public:
     bool isPalindrome(int x) {
-        return false;
+        if (x < 0)    return false;            //负数一定不是回文
+        
+        int len  = 1;                          //记录数的位数
+        int t = 1;                             //计算数究竟有多大
+        do {
+            t = 10 * t;
+            if (x < t)    break;
+            len++;
+        } while (t <= 100000000);
+
+        if (len != 10)    t = t / 10;
+
+        int tmp1 = x, tmp2 = x;
+        while (len>0) {
+            if (tmp1/t != tmp2%10)
+                return false;
+            else {
+                tmp1 = tmp1 % t;
+                tmp2 = tmp2 / 10;
+                len = len - 2;
+                t = t / 10;
+            }
+        }
+        return true;
+        //附上一个48ms完成的别人的代码，我的120ms，好吧别人的思路真的好，跪
+        /*int numSrc = x;
+        long long numDst = 0;
+        if (x < 0)
+        {
+            return false;
+        }
+
+        while (numSrc)
+        {
+            numDst = numDst * 10 + numSrc % 10;
+            numSrc /= 10;
+        }
+
+        return (numDst == x);*/
     }
 };
 
 int main() {
-    int x = 12321;
+    int x = 2147447412;
     cout << x << endl;
     cout << Solution().isPalindrome(x) << endl;
     return 0;
