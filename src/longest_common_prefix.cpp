e847210d3a558833c2070db066220d35efcdaa9f
mumumushi <yuanchenbiao@outlook.com>
2019-02-09 15:07:00 +0800

    Done longest_common_prefix.cpp很简单

--- src/longest_common_prefix.cpp
+++ src/longest_common_prefix.cpp
@@ -23,12 +23,33 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         string prefix = "";
+        int i = 0;
+        int j = 0;
+        char c = '\0';
+        int n = strs.size();
+
+        if (n == 0) return prefix;
+
+        while (true) {
+            if (j < strs[0].size())       //先提取第一项的第j个字符
+                c = strs[0][j];
+            else return prefix;
+
+            for (i = 1; i < n; i++) {     //对每一项的第j个字符进行判断
+                if (j >= strs[i].size())
+                    return prefix;
+                if (c != strs[i][j])
+                    return prefix;
+            }
+            prefix = prefix + c;
+            j++;
+        }
         return prefix;
     }
 };
 
 int main() {
-    vector<string> strs = {"flower", "flow", "flight"};
+    vector<string> strs = {"flower","flow","flight"};
     cout << Solution().longestCommonPrefix(strs) << endl;
     return 0;
 }
