cfa1f391bfcb7b84225fdc7befe6125036cd800c
mumumushi <yuanchenbiao@outlook.com>
2019-05-25 17:51:58 +0800

    添加generate_parentheses.cpp问题

---
+++ src/generate_parentheses.cpp
@@ -0,0 +1,29 @@
+/*
+Given n pairs of parentheses, write a function to generate all combinations 
+of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+*/
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        
+    }
+};
+
+int main() {
+    
+    
+    return 0;
+}
