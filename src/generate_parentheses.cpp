07cba49bf091ae71f15985337cd7f5b19c4e3046
mumumushi <yuanchenbiao@outlook.com>
2019-05-26 16:14:02 +0800

    回溯递归。。

--- src/generate_parentheses.cpp
+++ src/generate_parentheses.cpp
@@ -17,13 +17,34 @@ using namespace std;
 
 class Solution {
 public:
-    vector<string> generateParenthesis(int n) {
-        
+    vector<string> generateParenthesis(int n) {     //回溯递归
+        vector<string> ans;
+        if (n <= 0) {
+            ans.push_back("");
+            return ans;
+        }
+        generate(ans, "", 0, 0, n);
+        return ans;
+    }
+
+    void generate(vector<string>& v, string s, int left, int right, int n) {
+        if (left > n || right > n)    return ;
+        if (left == n && right == n)    v.push_back(s);
+
+        if (left >= right) {
+            string s1 = string(s);
+            generate(v, s+"(", left+1, right, n);
+            generate(v, s1+")", left, right+1, n);
+        }
     }
 };
 
 int main() {
-    
-    
+    int n = 0;
+    Solution s;
+    vector<string> ans = s.generateParenthesis(n);
+    for (auto i = ans.begin(); i != ans.end(); i++) {
+        cout << *i << endl;
+    }
     return 0;
 }
